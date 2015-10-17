29e51985e7a63e2591705fc78ae75ae71d805a73
Emanuel Danci <danci.emanuel@gmail.com>
2015-10-17 17:43:58 +0300

    Revert to old dataset name retrieval

--- zsync/location.py
+++ zsync/location.py
@@ -73,10 +73,7 @@ class S3Location(Location):
     self.bucket = self.host
 
     self.path = self._path.split("/")
-    if len(self.path) > 1 and not self.path[-1]:
-        self.dataset = self.path[-2]
-    else:
-        self.dataset = self.path[-1] 
+    self.dataset = self.path[-1]
     self.path = self.path[:-1]
 
     if self.dataset.find("@") != -1:
